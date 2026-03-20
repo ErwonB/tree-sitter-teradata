d14c99dcb6c1523a690bef9879564388f75f5dce
Erwan Bourre <github@bourre.me>
2026-03-20 13:55:14 +0100

    remove conflict on _collect_statement

--- grammar.js
+++ grammar.js
@@ -17,7 +17,6 @@ module.exports = grammar({
     [$.object_reference],
     [$.between_expression, $.period_expression, $.binary_expression],
     [$._expression_base, $.binary_expression],
-    [$._collect_statement],
     [$._inner_default_expression, $._one_word_function],
     [$._one_word_function],
   ],
@@ -864,25 +863,39 @@ module.exports = grammar({
     ),
 
     _collect_statement: $ => seq(
-      $.keyword_collect,
-      choice($._stats),
-      choice(
-        seq($.keyword_on, $.object_reference, $.keyword_column, field('value', $.object_reference)),
-        seq($.keyword_on, $.object_reference, $.keyword_column, wrapped_in_parenthesis(comma_list(field('value', $.object_reference)))),
-        seq(
-          $.keyword_column, wrapped_in_parenthesis(comma_list(field('value', $.object_reference))),
-          repeat(seq(',', $.keyword_column, wrapped_in_parenthesis(comma_list(field('value', $.object_reference))))),
-          $.keyword_on,
-          $.object_reference,
+        $.keyword_collect,
+        $._stats,
+        choice(
+          // Form: COLLECT STATS ON table COLUMN col
+          seq(
+            $.keyword_on, $.object_reference,
+            $.keyword_column, field('value', $.object_reference)
           ),
-        seq(
-          comma_list(seq($.keyword_column, field('value', $.object_reference))),
-          repeat(seq(',', $.keyword_column, field('value', $.object_reference))),
-          $.keyword_on,
-          $.object_reference,
-          )
+          // Form: COLLECT STATS ON table COLUMN (col, col, ...)
+          seq(
+            $.keyword_on, $.object_reference,
+            $.keyword_column, wrapped_in_parenthesis(comma_list(field('value', $.object_reference)))
+          ),
+          // Form: COLLECT STATS COLUMN (col,...) [, COLUMN (col,...)] ON table
+          seq(
+            $.keyword_column,
+            choice(
+              field('value', $.object_reference),
+              wrapped_in_parenthesis(comma_list(field('value', $.object_reference)))
+            ),
+            repeat(seq(
+              ',',
+              $.keyword_column,
+              choice(
+                field('value', $.object_reference),
+                wrapped_in_parenthesis(comma_list(field('value', $.object_reference)))
+              ),
+            )),
+            $.keyword_on,
+            $.object_reference,
+          ),
         )
-    ),
+      ),
 
     cte: $ => seq(
       $.identifier,
